4e6d2d1c285433aa64e16f9b025dd8714bd1eee8
Themba Msiza <tfmzworld@gmail.com>
2020-04-08 13:23:19 +0200

    Roll dice
    Add animation to dice and push final commit

--- src/Layout/RollDice/index.js
+++ src/Layout/RollDice/index.js
@@ -7,38 +7,52 @@ import './rollDice.css'
 
 class RollDice extends Component {
 
+    interval = 0;
+
     constructor(props) {
         super(props);
 
         this.state = {
             diceList: [2,3],
+            buttonPressTimer: 0,
+            rolling: false,
         }
-
+        this.onRollDice = this.onRollDice.bind(this);
     }
 
-    onRollDice = () =>  {
+    onRollDice () {
         let dice = this.state.diceList;
-    
-        dice = this.state.diceList.map( die => {
-            return Math.floor(Math.random()*6);
-        })
 
-        this.setState(() => {
-            return {diceList:dice}
-        });
+        this.setState({rolling: true})
+
+        setTimeout(() => {
+            dice = this.state.diceList.map( die => {
+                return Math.floor(Math.random()*6);
+            })
+            this.setState({rolling: false})
+            this.setState({diceList:dice, wobble:'wobble'});
+        }, 1500);
+
     }
 
     render() {
-        const { diceList } = this.state; 
+        const { diceList, rolling } = this.state; 
 
         const diceSection = diceList.map( die => {
             return <Dice key={Math.random()} count={diceNumerics[die]} ></Dice>
-        }) 
-        
+        });
+
         return (
             <div>
                 <div> {diceSection} </div>
-                <button className='btn-roll' onClick={this.onRollDice}>Roll Dice</button>
+
+                <button 
+                    className='btn-roll' 
+                    disabled={rolling? true : false }
+                    onClick={this.onRollDice}
+                >
+                    {rolling ? 'rolling...' : 'Roll Dice'}
+                </button>
             </div>
         )
     }
